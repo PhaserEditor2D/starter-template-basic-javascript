f4e877c0760e4d22ac93f6b7cc0f215ce4e960eb
PhaserEditor2D <developers@phasereditor2d.com>
2023-04-15 06:26:20 -0400

    Adds preload scripts.

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -23,9 +23,29 @@ class Preload extends Phaser.Scene {
 	editorCreate() {
 
 		// guapen
-		const guapen = this.add.image(400, 219, "guapen");
-		guapen.scaleX = 0.5915891440784282;
-		guapen.scaleY = 0.5915891440784282;
+		const guapen = this.add.image(505.0120544433594, 360, "guapen");
+		guapen.scaleX = 0.32715486817515643;
+		guapen.scaleY = 0.32715486817515643;
+
+		// progressBar
+		const progressBar = this.add.rectangle(553.0120849609375, 361, 256, 20);
+		progressBar.setOrigin(0, 0);
+		progressBar.isFilled = true;
+		progressBar.fillColor = 14737632;
+
+		// preloadUpdater
+		new PreloadBarUpdaterScript(progressBar);
+
+		// progressBarBg
+		const progressBarBg = this.add.rectangle(553.0120849609375, 361, 256, 20);
+		progressBarBg.setOrigin(0, 0);
+		progressBarBg.fillColor = 14737632;
+		progressBarBg.isStroked = true;
+
+		// loadingText
+		const loadingText = this.add.text(552.0120849609375, 329, "", {});
+		loadingText.text = "Loading...";
+		loadingText.setStyle({ "color": "#e0e0e0", "fontFamily": "arial", "fontSize": "20px" });
 
 		this.events.emit("scene-awake");
 	}
